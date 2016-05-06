e05eaa99aa911096a92bedb8d3bca557cd95b0fc
Anoop <anoopg87@gmail.com>
2016-05-06 17:09:58 +1000

    check some

--- app/src/main/java/com/android/phonestatelistener/MainActivity.java
+++ app/src/main/java/com/android/phonestatelistener/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.phonestatelistener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneStateListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PhoneStateListener phoneStateListener;
 
-            }
+    }
 }
