1b412f8d22edeef786dbffb8fedc97d187b9cd2d
Anoop <anoopg87@gmail.com>
2016-05-06 16:56:09 +1000

    // Added comment

--- app/src/main/java/com/android/phonestatelistener/MainActivity.java
+++ app/src/main/java/com/android/phonestatelistener/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Ne feature to be added here
     }
 }
